48b393a7480189a6abe5f21de760266faf838813
Amo1000 <aymoncoccaro@gmail.com>
2025-06-03 21:04:02 +1000

    more updates
    sidebar, loader and rss pages css updated

--- all.js
+++ all.js
@@ -1,5 +1,6 @@
 document.getElementById('profile-options').insertAdjacentHTML("beforeend",'<a class="icon-folder" href="/groups#my-favourite-groups">Favourite groups</a></li>')
 document.getElementById('profile-options').insertAdjacentHTML("beforeend",'<a class="icon-link" href="/cms/myLinks">My links</a></li>')
+document.getElementById('profile-options').insertAdjacentHTML("beforeend",'<a class="icon-star" href="/news/saved">Saved news</a></li>')
 document.getElementById('profile-options').insertAdjacentHTML("beforeend",'<a href="/resources/" class="icon-files">Resources</a></li>')
 document.getElementById('footer').insertAdjacentHTML("afterbegin",'<a onclick="document.documentElement.scrollTop = 0;" id="myBtn" title="Go to top">Back to top</a>')
 
@@ -14,3 +15,4 @@ const active1 = document.querySelectorAll(".breadcrumb li:nth-child(1)");
 for (var i = 0; i < active.length; i++ ) {
     active1[i].insertAdjacentHTML("afterbegin",'<a href="https://skcportal.stkevins.vic.edu.au/"><strong> ~ </strong></a>')
 }
+
